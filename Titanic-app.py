d15989bf7060516e4d2531cf82744ab0bc558dfe
yanjin1225 <149980713+yanjin1225@users.noreply.github.com>
2024-09-27 01:22:20 +0800

    Update Titanic-app.py

--- Titanic-app.py
+++ Titanic-app.py
@@ -4,19 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# 显示标题
 st.title('Titanic App by JIN YAN')
-
-# 读取Titanic数据集
 df = pd.read_csv('train.csv')
-
-# 显示数据集
 st.write(df)
 
-# 创建三个子图的图形
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-
-# 绘制不同等级的箱图
 for i, pclass in enumerate([1, 2, 3]):
     sns.boxplot(x='Pclass', y='Fare', data=df[df['Pclass'] == pclass], ax=axes[i], width=0.2)
     axes[i].set_xlabel(f'PClass={pclass}')
@@ -24,11 +16,8 @@ for i, pclass in enumerate([1, 2, 3]):
     axes[i].set_xticks([0])
     axes[i].set_xticklabels(['Fare'])
     axes[i].grid(True)
-
-# 调整布局
+    
 plt.tight_layout()
-
-# 在Streamlit中显示图形
 st.pyplot(fig)
 
 
